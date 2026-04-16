226a78b5c603ee304a987b790debe0b69f8cc0da
Ruediger Klaehn <rklaehn@protonmail.com>
2026-04-16 16:33:26 +0300

    Patch iroh to main

--- src/main.rs
+++ src/main.rs
@@ -294,7 +294,7 @@ fn get_or_create_secret(print: bool) -> anyhow::Result<SecretKey> {
     match std::env::var("IROH_SECRET") {
         Ok(secret) => SecretKey::from_str(&secret).context("invalid secret"),
         Err(_) => {
-            let key = SecretKey::generate(&mut rand::rng());
+            let key = SecretKey::generate();
             if print {
                 let key = hex::encode(key.to_bytes());
                 eprintln!("using secret key {key}");
